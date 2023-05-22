ac688f1660ccd9dc4b09198c94ff88c4fd644871
CATHAYSA NAVARRO BENÍTEZ <catnavben@alu.edu.gva.es>
2023-05-22 20:27:45 +0200

    Hello World!

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.DNI = new System.Windows.Forms.Label();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.telefono = new System.Windows.Forms.Label();
+            this.Apellidos = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bUltimo = new System.Windows.Forms.Button();
+            this.bSiguiente = new System.Windows.Forms.Button();
+            this.bAnterior = new System.Windows.Forms.Button();
+            this.bPrimero = new System.Windows.Forms.Button();
+            this.bAnyadir = new System.Windows.Forms.Button();
+            this.bguardar = new System.Windows.Forms.Button();
+            this.bActualizar = new System.Windows.Forms.Button();
+            this.bEliminar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(125, 85);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(100, 22);
+            this.txtDni.TabIndex = 0;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(125, 174);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 22);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(125, 264);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(100, 22);
+            this.txtTelefono.TabIndex = 2;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(439, 174);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(100, 22);
+            this.txtApellidos.TabIndex = 3;
+            this.txtApellidos.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(439, 264);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 22);
+            this.txtEmail.TabIndex = 4;
+            // 
+            // DNI
+            // 
+            this.DNI.AutoSize = true;
+            this.DNI.Location = new System.Drawing.Point(37, 85);
+            this.DNI.Name = "DNI";
+            this.DNI.Size = new System.Drawing.Size(30, 16);
+            this.DNI.TabIndex = 5;
+            this.DNI.Text = "DNI";
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(40, 179);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(56, 16);
+            this.Nombre.TabIndex = 6;
+            this.Nombre.Text = "Nombre";
+            // 
+            // telefono
+            // 
+            this.telefono.AutoSize = true;
+            this.telefono.Location = new System.Drawing.Point(40, 270);
+            this.telefono.Name = "telefono";
+            this.telefono.Size = new System.Drawing.Size(29, 16);
+            this.telefono.TabIndex = 7;
+            this.telefono.Text = "Tlfn";
+            // 
+            // Apellidos
+            // 
+            this.Apellidos.AutoSize = true;
+            this.Apellidos.Location = new System.Drawing.Point(357, 174);
+            this.Apellidos.Name = "Apellidos";
+            this.Apellidos.Size = new System.Drawing.Size(64, 16);
+            this.Apellidos.TabIndex = 8;
+            this.Apellidos.Text = "Apellidos";
+            // 
+            // email
+            // 
+            this.email.AutoSize = true;
+            this.email.Location = new System.Drawing.Point(357, 264);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(40, 16);
+            this.email.TabIndex = 9;
+            this.email.Text = "email";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bEliminar);
+            this.groupBox1.Controls.Add(this.bActualizar);
+            this.groupBox1.Controls.Add(this.bguardar);
+            this.groupBox1.Controls.Add(this.bAnyadir);
+            this.groupBox1.Controls.Add(this.bUltimo);
+            this.groupBox1.Controls.Add(this.bSiguiente);
+            this.groupBox1.Controls.Add(this.bAnterior);
+            this.groupBox1.Controls.Add(this.bPrimero);
+            this.groupBox1.Location = new System.Drawing.Point(54, 328);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(523, 163);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Navegar";
+            // 
+            // bUltimo
+            // 
+            this.bUltimo.Location = new System.Drawing.Point(356, 40);
+            this.bUltimo.Name = "bUltimo";
+            this.bUltimo.Size = new System.Drawing.Size(96, 39);
+            this.bUltimo.TabIndex = 3;
+            this.bUltimo.Text = "Último";
+            this.bUltimo.UseVisualStyleBackColor = true;
+            this.bUltimo.Click += new System.EventHandler(this.bUltimo_Click);
+            // 
+            // bSiguiente
+            // 
+            this.bSiguiente.Location = new System.Drawing.Point(247, 40);
+            this.bSiguiente.Name = "bSiguiente";
+            this.bSiguiente.Size = new System.Drawing.Size(96, 39);
+            this.bSiguiente.TabIndex = 2;
+            this.bSiguiente.Text = "Siguiente";
+            this.bSiguiente.UseVisualStyleBackColor = true;
+            this.bSiguiente.Click += new System.EventHandler(this.bSiguiente_Click);
+            // 
+            // bAnterior
+            // 
+            this.bAnterior.Location = new System.Drawing.Point(136, 40);
+            this.bAnterior.Name = "bAnterior";
+            this.bAnterior.Size = new System.Drawing.Size(93, 39);
+            this.bAnterior.TabIndex = 1;
+            this.bAnterior.Text = "Anterior";
+            this.bAnterior.UseVisualStyleBackColor = true;
+            this.bAnterior.Click += new System.EventHandler(this.bAnterior_Click);
+            // 
+            // bPrimero
+            // 
+            this.bPrimero.Location = new System.Drawing.Point(19, 40);
+            this.bPrimero.Name = "bPrimero";
+            this.bPrimero.Size = new System.Drawing.Size(98, 39);
+            this.bPrimero.TabIndex = 0;
+            this.bPrimero.Text = "primero";
+            this.bPrimero.UseVisualStyleBackColor = true;
+            this.bPrimero.Click += new System.EventHandler(this.bPrimero_Click);
+            // 
+            // bAnyadir
+            // 
+            this.bAnyadir.Location = new System.Drawing.Point(19, 95);
+            this.bAnyadir.Name = "bAnyadir";
+            this.bAnyadir.Size = new System.Drawing.Size(98, 33);
+            this.bAnyadir.TabIndex = 4;
+            this.bAnyadir.Text = "Añadir";
+            this.bAnyadir.UseVisualStyleBackColor = true;
+            this.bAnyadir.Click += new System.EventHandler(this.bAnyadir_Click);
+            // 
+            // bguardar
+            // 
+            this.bguardar.Location = new System.Drawing.Point(130, 95);
+            this.bguardar.Name = "bguardar";
+            this.bguardar.Size = new System.Drawing.Size(99, 33);
+            this.bguardar.TabIndex = 5;
+            this.bguardar.Text = "Guardar";
+            this.bguardar.UseVisualStyleBackColor = true;
+            this.bguardar.Click += new System.EventHandler(this.bguardar_Click);
+            // 
+            // bActualizar
+            // 
+            this.bActualizar.Location = new System.Drawing.Point(247, 95);
+            this.bActualizar.Name = "bActualizar";
+            this.bActualizar.Size = new System.Drawing.Size(96, 33);
+            this.bActualizar.TabIndex = 6;
+            this.bActualizar.Text = "Actualizar";
+            this.bActualizar.UseVisualStyleBackColor = true;
+            this.bActualizar.Click += new System.EventHandler(this.bActualizar_Click);
+            // 
+            // bEliminar
+            // 
+            this.bEliminar.Location = new System.Drawing.Point(356, 95);
+            this.bEliminar.Name = "bEliminar";
+            this.bEliminar.Size = new System.Drawing.Size(96, 33);
+            this.bEliminar.TabIndex = 7;
+            this.bEliminar.Text = "Eliminar";
+            this.bEliminar.UseVisualStyleBackColor = true;
+            this.bEliminar.Click += new System.EventHandler(this.bEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(840, 535);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.Apellidos);
+            this.Controls.Add(this.telefono);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.DNI);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtDni);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label DNI;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.Label telefono;
+        private System.Windows.Forms.Label Apellidos;
+        private System.Windows.Forms.Label email;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button bUltimo;
+        private System.Windows.Forms.Button bSiguiente;
+        private System.Windows.Forms.Button bAnterior;
+        private System.Windows.Forms.Button bPrimero;
+        private System.Windows.Forms.Button bguardar;
+        private System.Windows.Forms.Button bAnyadir;
+        private System.Windows.Forms.Button bEliminar;
+        private System.Windows.Forms.Button bActualizar;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Runtime.Remoting.Contexts;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataSet dsProfesores;
+        SqlDataAdapter da;
+        //variable que indica en qué registro estamos situados.
+
+        private int pos;
+        private int maxRegistros;
+
+        private void mostrarRegistro (int pos)
+        {
+            //Objeto que nos permite recoger un registro de la tabla.
+            DataRow dRegistro;
+
+            // Cogemos el registro de la posición pos en la tabla Profesores
+            dRegistro = dsProfesores.Tables["Profesores"].Rows[pos];
+
+            //Cogemos el valor de cada una de las columnas del registro y lo ponemos en el Txt correspondiente,
+
+            txtDni.Text = dRegistro[0].ToString();
+            txtNombre.Text = dRegistro[1].ToString();   
+            txtApellidos.Text = dRegistro[2].ToString();
+            txtTelefono.Text = dRegistro[3].ToString();
+            txtEmail.Text = dRegistro[4].ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cadenaConexion = "Data Source = (LocalDB)\\MSSQLLocalDB;" +
+               "AttachDbFilename = C:\\Users\\Cai\\OneDrive\\Escritorio\\DAW\\PROGRAMACIÓN\\tema 9\\ejercicios\\Instituto.mdf;" +
+               "Integrated Security = True; Connect Timeout = 30";
+
+            SqlConnection con = new SqlConnection(cadenaConexion);
+
+            //abrimos la conexión
+            con.Open();
+
+            dsProfesores = new DataSet();
+            string cadenaSql = "SELECT * From Profesores";
+
+            da = new SqlDataAdapter(cadenaSql, con);
+
+            da.Fill(dsProfesores, "Profesores");
+
+            //Situamos la primera posición.
+
+            pos = 0;
+            mostrarRegistro(pos);
+
+            //Obtenemos el número de registros
+            maxRegistros = dsProfesores.Tables["Profesores"].Rows.Count;
+             
+            //cerramos la conexión
+            con.Close();
+
+            
+
+        }
+
+        private void bPrimero_Click(object sender, EventArgs e)
+        {
+            // Ponemos la primera posición
+            pos = 0;
+            mostrarRegistro(pos);
+        }
+
+        private void bAnterior_Click(object sender, EventArgs e)
+        {
+            //Para saber qué tipo de objetco es sender
+            Console.WriteLine(sender.GetType().Name);
+
+            //He cambiado el tipo del objeto sender a botón (castear)
+            Button btn = sender as Button;
+
+
+            // Vamos a la posición anterior.
+            pos--;
+            if (pos<0)
+            {   //descativamos el botón
+                btn.Enabled = false;
+            }
+            else
+            {   //activamos el botón
+                btn.Enabled = true;
+                mostrarRegistro(pos);
+            }
+            
+        }
+
+        private void bSiguiente_Click(object sender, EventArgs e)
+        {
+          
+            //He cambiado el tipo del objeto sender a botón (castear)
+            Button btn = sender as Button;
+
+            // Vamos a la posición siguiente
+            pos++;
+            if (pos == maxRegistros)
+            {   //desactivamos el botón
+                btn.Enabled = false; 
+            } 
+
+            else
+            {   //activamos el botón
+                btn.Enabled= true;
+                mostrarRegistro(pos);
+                
+            }
+
+            
+        }
+
+        private void bUltimo_Click(object sender, EventArgs e)
+        {
+            // Vamos a la última posición.
+            // Los registros van del 0 al numero de registros - 1
+            pos = maxRegistros - 1;
+            mostrarRegistro(pos);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bAnyadir_Click(object sender, EventArgs e)
+        {
+            txtDni.Clear();
+            txtNombre.Clear();
+            txtApellidos.Clear();
+            txtTelefono.Clear();
+            txtEmail.Clear();
+        }
+
+        private void bguardar_Click(object sender, EventArgs e)
+        {
+            //Creamos un nuevo registro.
+            DataRow dRegistro = dsProfesores.Tables["Profesores"].NewRow();
+
+            //Metemos los datos en el nuevo registro
+            dRegistro[0] = txtDni.Text;
+            dRegistro[1] = txtNombre.Text;
+            dRegistro[2] = txtApellidos.Text;
+            dRegistro[3] = txtTelefono.Text;
+            dRegistro[4] = txtEmail.Text;
+
+
+            //Añadimos el registro al Dataset.
+            dsProfesores.Tables["Profesores"].Rows.Add(dRegistro);
+
+            //Reconectamos con el dataAdapter y acutalizamos la Base de Datos.
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+            da.Update(dsProfesores, "Profesores");
+
+            //Actualizamos el número de registros y la posición en la tabla.
+            maxRegistros++;
+            pos = maxRegistros - 1;
+        }
+
+        private void bActualizar_Click(object sender, EventArgs e)
+        {
+            //Cogemos el registro situado en la posición actual.
+            DataRow dRegistro = dsProfesores.Tables["Profesores"].Rows[pos];
+
+            //Metemos los datos en el registro
+            dRegistro[0] = txtDni.Text;
+            dRegistro[1] = txtNombre.Text;
+            dRegistro[2] = txtApellidos.Text;
+            dRegistro[3] = txtTelefono.Text;
+            dRegistro[4] = txtEmail.Text;
+
+            //Reconectamos con el dataAdapter y actualizamos la Base de Datos.
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+            da.Update(dsProfesores, "Profesores");
+        }
+
+        private void bEliminar_Click(object sender, EventArgs e)
+        {
+            //Eliminamos el registro situado en la posición actual.
+            dsProfesores.Tables["Profesores"].Rows[pos].Delete();
+
+            //Tenemos un registro menos.
+            maxRegistros--;
+
+            //Vamos al primer registro y lo mostramos
+            pos = 0;
+            mostrarRegistro(pos);
+
+            //Reconectamos con el dataAdapter y actualizamos la Base de Datos.
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+            da.Update(dsProfesores, "Profesores");
+
+        }
+    }
+}
